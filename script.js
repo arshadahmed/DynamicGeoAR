f735ae9be9e9323de469c88c4665c668e4c5545f
Arsh <arshad.s.ahmed@hotmail.com>
2020-02-20 13:57:37 +1100

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function renderPlaces(places) {
         icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
         icon.setAttribute('name', place.name);
         icon.setAttribute('city', place.city);
-        icon.setAttribute('src', '/assets/map-marker.png');
+        icon.setAttribute('src', 'https://www.w3schools.com/images/picture.jpg');
 
         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
         icon.setAttribute('scale', '10, 10');
